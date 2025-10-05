c64974d57decfcb2b37ad7446863179f0131a567
Hao Ran Lai <hrlai.ecology@gmail.com>
2025-10-05 13:41:52 +1300

    polish conditionals

--- R/usetemplate.R
+++ R/usetemplate.R
@@ -10,6 +10,7 @@
 #'   When it is NULL (the default), it will use the built-in template directory.
 #' @param template A character string of the template filename, without the `.R`
 #'   extension.
+#' @param overwrite Logical; whether to overwrite an existing R script or not.
 #'
 #' @returns An R script stored in the `path_dest` directory.
 #' @export
@@ -18,23 +19,38 @@
 #' \dontrun{usetemplate("test", template = "test_template")}
 
 usetemplate <- function(
-    filename,
-    path_dest = ".",
-    path_template = NULL,
-    template = NULL) {
-
-  template <- paste0(template, ".R")
-
+  filename,
+  path_dest = ".",
+  path_template = NULL,
+  template = NULL,
+  overwrite = FALSE
+) {
+  # use the built-in template directory if nothing is supplied
   if (is.null(path_template)) {
-    path_template <- system.file("templates", template, package = "usetemplate")
+    path_template <- system.file("templates", package = "usetemplate")
   }
 
-  if (path_template == "") {
-    cli::cli_alert_danger("Template not found in package.")
+  path_template_full <- file.path(path_template, paste0(template, ".R"))
+
+  if (!file.exists(path_template_full)) {
+    cli::cli_alert_danger("Template not found!")
   }
 
-  path_dest <- file.path(path_dest, paste0(filename, ".R"))
-  file.copy(path_template, path_dest, overwrite = FALSE)
+  path_dest_full <- file.path(path_dest, paste0(filename, ".R"))
 
-  cli::cli_alert_success("File created at {path_dest}.")
+  # generate R script
+  if (file.exists(path_dest_full)) {
+    if (!overwrite) {
+      cli::cli_alert_danger(
+        "Skipping copy because destination file already exists."
+      )
+      cli::cli_alert_info("To overwrite, set {.code overwrite = TRUE}")
+    } else {
+      file.copy(path_template_full, path_dest_full, overwrite = TRUE)
+      cli::cli_alert_warning("File overwritten at {path_dest}.")
+    }
+  } else {
+    file.copy(path_template_full, path_dest_full, overwrite = FALSE)
+    cli::cli_alert_success("File created at {path_dest}.")
+  }
 }
